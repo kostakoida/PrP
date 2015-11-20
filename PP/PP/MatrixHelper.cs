de7d7a933a634866dd961106758531c0bda80579
PC\koste_000 <pko@compent.net>
2015-11-20 16:33:24 +0200

    fix equals for AVX

--- PP/PP/MatrixHelper.cs
+++ PP/PP/MatrixHelper.cs
@@ -1,34 +1,51 @@
 ﻿using System;
+using System.Linq;
 using System.Numerics;
 
 namespace PP
 {
     public class MatrixHelper
     {
-        private readonly static int VectorSize = Vector<float>.Count;
-
-        public static bool IsSimdVectorEqualNotSimd(float[] woutSimd, Vector<float>[] withSimd)
+        public static bool IsSimdVectorEqualNotSimd(float[] woutSimd, Vector4[] withSimd)
         {
             for (var i = 0; i < withSimd.Length; i++)
             {
-                for (var j = 0; j < VectorSize; j++)
+                var diffX = Math.Pow(10, GetError(woutSimd[i * 4], withSimd[i].X));
+                var diffY = Math.Pow(10, GetError(woutSimd[i * 4 + 1], withSimd[i].Y));
+                var diffW = Math.Pow(10, GetError(woutSimd[i * 4 + 2], withSimd[i].W));
+                var diffZ = Math.Pow(10, GetError(woutSimd[i * 4 + 3], withSimd[i].Z));
+
+
+                if (Math.Max(woutSimd[i * 4], withSimd[i].X) - Math.Min(woutSimd[i], withSimd[i].X) > diffX
+                && Math.Max(woutSimd[i * 4 + 1], withSimd[i].Y) - Math.Min(woutSimd[i], withSimd[i].Y) > diffY
+                && Math.Max(woutSimd[i * 4 + 2], withSimd[i].W) - Math.Min(woutSimd[i], withSimd[i].W) > diffW
+                && Math.Max(woutSimd[i * 4 + 3], withSimd[i].Z) - Math.Min(woutSimd[i], withSimd[i].Z) > diffZ)
                 {
-                    var diffX = Math.Pow(10, GetError(woutSimd[i * VectorSize], withSimd[i][j]));
-                    if (Math.Max(woutSimd[i * VectorSize], withSimd[i][j]) - Math.Min(woutSimd[i], withSimd[i][j]) > diffX)
-                    {
-                        return false;
-                    }
+                    return false;
                 }
             }
             return true;
         }
 
 
-        public static int GetError(float a, float b)
+        public static double GetError(float a, float b)
+        {
+            var diff = Math.Abs(a) > Math.Pow(10, 7) ? GetBigError(a, b) : GetSmallError(a, b);
+            if (diff > Math.Abs(a) / 10)
+                Console.WriteLine($"!!!!!!!!!!!!!!!!!!!!!!!!!!!10% of {diff} not smaller that {Math.Abs(a) / 10}");
+            return diff;
+        }
+
+        private static float GetSmallError(float a, float b)
+        {
+            return Math.Abs(a - b) / Math.Max(Math.Abs(a), Math.Abs(b));
+        }
+
+        private static double GetBigError(float a, float b)
         {
             if (a < b)
             {
-                return GetError(b, a);
+                return GetBigError(b, a);
             }
             var isEnd = 0;
             var res = 1;
@@ -41,26 +58,12 @@ namespace PP
                 if (a < 10) isEnd++;
             }
             res += res >= 7 ? 1 : 0;
-            return res;
+            return Math.Pow(10, res);
         }
 
         public static bool IsVectorsEqual(float[] a, float[] b)
         {
-            for (var i = 0; i < a.Length; i++)
-            {
-                var max = a[i] >= b[i] ? a[i] : b[i];
-                var min = a[i] >= b[i] ? b[i] : a[i];
-                if (max - min != 0)
-                    Console.Write("");
-                var diff = Math.Pow(10, GetError(a[i], b[i]));
-
-                if (max - min > diff)
-                {
-                    return false;
-                }
-            }
-
-            return true;
+            return !a.Where((t, i) => Math.Abs(t - b[i]) > GetError(t, b[i])).Any();
         }
 
         public static bool IsEqual(float[,] matrix1, float[,] matrix2, int size)
@@ -69,13 +72,7 @@ namespace PP
             {
                 for (var j = 0; j < size; j++)
                 {
-                    var max = matrix1[i, j] >= matrix2[i, j] ? matrix1[i, j] : matrix2[i, j];
-                    var min = matrix1[i, j] >= matrix2[i, j] ? matrix2[i, j] : matrix1[i, j];
-                    if (max - min != 0)
-                        Console.Write("");
-                    var diff = Math.Pow(10, GetError(matrix1[i, j], matrix2[i, j]));
-
-                    if (max - min > diff)
+                    if (Math.Abs(matrix1[i, j] - matrix2[i, j]) > GetError(matrix1[i, j], matrix2[i, j]))
                     {
                         return false;
                     }
